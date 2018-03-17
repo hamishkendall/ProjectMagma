9468994473d8bf11e4170f68e70b468d60bb5d4f
James Stewart <auminist3@gmail.com>
2018-03-17 13:02:13 +1300

    Pause/Resume script

---
+++ PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+	}
+
+    void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+        Debug.Log("Game was resumed");
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+        Debug.Log("Game was paused");
+    }
+}
