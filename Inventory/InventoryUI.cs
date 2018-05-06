8ade1fc8bf635f00b5ba1c885c6a98ec2dbb5627
hoon9205 <37890711+hoon9205@users.noreply.github.com>
2018-05-06 16:48:13 +1200

    Sprint 1 script Taehoon Chung

---
+++ Inventory/InventoryUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class InventoryUI : MonoBehaviour {
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    Inventory inventory;
+
+    InventorySlot[] slots;
+	
+	void Start () {
+        inventoryUI.SetActive(false); // Inventory is closed at the start
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+	}
+
+	void Update () {
+       
+         //Opening and closing inventory with the key I
+         if (Input.GetKeyDown(KeyCode.I))
+         {
+            Debug.Log("opennnnn");
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+         }
+	} 
+
+    void UpdateUI()
+    {        
+        //Updating inventory
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if(i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
