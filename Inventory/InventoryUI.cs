0ea5d3454217d2c497d794e30e749e3ba4fb3258
hoon9205 <37890711+hoon9205@users.noreply.github.com>
2018-05-06 18:44:56 +1200

    Sprint 1 Scripte Taehoon Chung
    Comments added

--- Inventory/InventoryUI.cs
+++ Inventory/InventoryUI.cs
@@ -4,29 +4,27 @@ public class InventoryUI : MonoBehaviour {
     public Transform itemsParent;
     public GameObject inventoryUI;
     Inventory inventory;
-
     InventorySlot[] slots;
 	
 	void Start () {
-        inventoryUI.SetActive(false); // Inventory is closed at the start
+        inventoryUI.SetActive(false); // This hides the inventory at the start
         inventory = Inventory.instance;
-        inventory.onItemChangedCallback += UpdateUI;
-        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+        inventory.onItemChangedCallback += UpdateUI; // Call UpdateUI method when there is a change in inventory
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();//Finding all the slots under itemsParent
 	}
 
 	void Update () {
        
          //Opening and closing inventory with the key I
          if (Input.GetKeyDown(KeyCode.I))
-         {
-            Debug.Log("opennnnn");
+         {           
             inventoryUI.SetActive(!inventoryUI.activeSelf);
          }
 	} 
 
     void UpdateUI()
     {        
-        //Updating inventory
+        //Updating inventory UI
         for (int i = 0; i < slots.Length; i++)
         {
             if(i < inventory.items.Count)
--- Inventory/ItemPickup.cs
+++ Inventory/ItemPickup.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour {
 
     private bool isInRange;
-    [SerializeField] KeyCode itemPickup = KeyCode.X;
+    [SerializeField] KeyCode itemPickup = KeyCode.X;// Key for picking up item
     public Item item;
     public Inventory inventory;
 
     private void Update()
     {
         if (isInRange && Input.GetKeyDown(itemPickup))
-        {
-            Debug.Log("Picking up" + item.ItemName);
-            Inventory.instance.AddItem(item);
-            Destroy(gameObject);
+        {            
+            Inventory.instance.AddItem(item);//Adding item to inventory
+            Destroy(gameObject);//Destroying the object after pick up
         }
     }
-
+    //Checking if the object is in range with the player
     private void OnTriggerEnter2D(Collider2D other)
     {
        isInRange = true;
